069d4357656fc18fc15bfdb58a52725cf88f9857
Dave Kiss <dave@davekiss.com>
2018-02-14 10:28:56 -0500

    Use the head of the default page as the active video

--- src/cli/themes/templates/src/index.js
+++ src/cli/themes/templates/src/index.js
@@ -31,7 +31,7 @@ const render = (Component, galleryId, store) => {
 
   const mount = '#vimeography-gallery-' + galleryId + ' > div';
   const gallery = window.vimeography2.galleries.<%= name.toLowerCase() %>[galleryId];
-  const firstVideoId = head( gallery.order );
+  const firstVideoId = head( gallery.pages.default[Object.keys(gallery.pages.default)[0]] );
   const activeVideoId = params.get('vimeography_video') && params.get('vimeography_gallery') == galleryId ? parseInt( params.get('vimeography_video') ) : parseInt( firstVideoId );
 
   store.commit({
