84f8a550a194230c520391a3323ddd80cd44cf2c
Dave Kiss <dave@davekiss.com>
2018-03-23 10:19:44 -0400

    Update thumbnail template to use mixin

--- src/cli/themes/templates/src/components/Thumbnail.vue
+++ src/cli/themes/templates/src/components/Thumbnail.vue
@@ -1,5 +1,6 @@
 <script>
 import { mapState } from 'vuex'
+import { Mixins } from 'vimeography-blueprint'
 
 const defaultTemplate = `
   <figure class="swiper-slide vimeography-thumbnail">
@@ -13,6 +14,7 @@ const userTemplate = document.querySelector('#vimeography-<%= name.toLowerCase()
 
 const Thumbnail = {
   props: ['video'],
+  mixins: [Mixins.Thumbnail],
   template: userTemplate ? userTemplate.innerText : defaultTemplate,
   computed: {
     query() {
@@ -23,11 +25,6 @@ const Thumbnail = {
       };
 
       return '?' + Object.keys(q).map(k => k + '=' + encodeURIComponent(q[k])).join('&')
-    },
-    thumbnailUrl: (data) => {
-      const selections = data.video.pictures.sizes.filter(img => img.width <= 640)
-      const sorted = selections.sort( (a, b) => a.width - b.width )
-      return sorted[sorted.length - 1].link
     },
     ...mapState({
       galleryId: state => state.id
