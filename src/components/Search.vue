1eb2abd5459dd58d8bbfb03e29967f7177c73bd1
Dave Kiss <dave@davekiss.com>
2018-04-07 13:27:43 -0400

    Add placeholder text to searchbox

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -7,7 +7,7 @@ import debounce from 'lodash/debounce';
 const template = `
   <label :class="$style.container">
     <span :class="$style.text">Search:</span>
-    <input :class="$style.input" name="search" type="text" v-on:input="debounceInput" />
+    <input :class="$style.input" name="search" type="text" placeholder="Search videos…" v-on:input="debounceInput" />
 
     <div :class="$style.icon">
       <svg v-show="!this.searching" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line>
