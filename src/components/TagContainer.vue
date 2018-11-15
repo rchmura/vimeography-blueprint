af4f254a36ce538bd8e745619c1991dc804ae35a
Dave Kiss <dave@davekiss.com>
2018-11-15 12:07:36 -0500

    Implement tag queries by watching route for param

--- src/components/TagContainer.vue
+++ src/components/TagContainer.vue
@@ -1,10 +1,10 @@
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions, mapGetters } from 'vuex'
 import Tag from './Tag.vue';
 
 const template = `
   <div class="vimeography-tag-container">
-    <tag v-for="tag in tags" :key="tag.name" :name="tag.name" :total="tag.total"></tag>
+    <tag v-for="tag in tags" :key="tag.name" :name="tag.name" :canonical="tag.canonical" :total="tag.total"></tag>
   </div>
 `;
 
@@ -12,10 +12,41 @@ const TagContainer = {
   template,
   computed: mapState({
     tags: state => state.gallery.tags,
+    galleryId: state => state.gallery.id,
+    tagsEnabled: state => state.gallery.settings.tags.enabled
   }),
   components: {
     Tag,
   },
+  methods: {
+    ...mapActions([
+      'performSearch'
+    ]),
+    ...mapGetters([
+      'getVideosByTag'
+    ]),
+    clearRoute (event) {
+      this.$router.push(window.location.pathname);
+    },
+  },
+  watch: {
+    '$route' (to, from) {
+      const tag = to.query.vimeography_tag;
+      const gallery = to.query.vimeography_gallery;
+
+      if (tag && gallery && gallery == this.galleryId) {
+        this.performSearch(tag);
+      }
+    }
+  },
+  mounted: function() {
+    const tag = this.$route.query.vimeography_tag;
+    const gallery = this.$route.query.vimeography_gallery;
+
+    if (tag && gallery && gallery == this.galleryId) {
+      this.performSearch(tag);
+    }
+  }
 }
 
 export default TagContainer;
