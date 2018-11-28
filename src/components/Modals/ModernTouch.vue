53395df809f91996a0dfae959a540941260c2598
Dave Kiss <dave@davekiss.com>
2018-11-28 08:53:13 -0500

    Change close element to an anchor

--- src/components/Modals/ModernTouch.vue
+++ src/components/Modals/ModernTouch.vue
@@ -14,9 +14,9 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="4" y1="12" x2="20" y2="12"></line><polyline points="14 6 20 12 14 18"></polyline></svg>
           </router-link>
 
-          <div @click="$modal.hide(modalId)">
+          <a href="#" @click="$modal.hide(modalId)">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-          </div>
+          </a>
         </div>
       </div>
 
