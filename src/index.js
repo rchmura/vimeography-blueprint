2a5eb9a7ee027de274fff7e2178bf93dd072838f
Dave Kiss <dave@davekiss.com>
2021-11-07 10:36:12 -0500

    Explicit exports

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@
  *
  */
 
-export Player from './components/Player.vue';
-export Lightbox from './components/Lightbox.vue';
-export Search from './components/Search.vue';
-export Filters from './components/Filters.vue';
-export DownloadLink from './components/DownloadLink.vue';
+import Player from './components/Player.vue';
+import Lightbox from './components/Lightbox.vue';
+import Search from './components/Search.vue';
+import Filters from './components/Filters.vue';
+import DownloadLink from './components/DownloadLink.vue';
 
-export PagingControls from './components/Paging'
-export storeModules from './store';
-export Mixins from './mixins';
+import PagingControls from './components/Paging'
+import storeModules from './store';
+import Mixins from './mixins';
+
+export default { Player, Lightbox, Search, Filters, DownloadLink, PagingControls, storeModules, Mixins };
