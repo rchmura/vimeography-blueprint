467a0d9c21b15276378cc2a1ab12d760e62ddb32
Dave Kiss <dave@davekiss.com>
2021-11-07 10:42:14 -0500

    Undo

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@
  *
  */
 
-import Player from './components/Player.vue';
-import Lightbox from './components/Lightbox.vue';
-import Search from './components/Search.vue';
-import Filters from './components/Filters.vue';
-import DownloadLink from './components/DownloadLink.vue';
+export Player from './components/Player.vue';
+export Lightbox from './components/Lightbox.vue';
+export Search from './components/Search.vue';
+export Filters from './components/Filters.vue';
+export DownloadLink from './components/DownloadLink.vue';
 
-import PagingControls from './components/Paging'
-import storeModules from './store';
-import Mixins from './mixins';
-
-export default { Player, Lightbox, Search, Filters, DownloadLink, PagingControls, storeModules, Mixins };
+export PagingControls from './components/Paging'
+export storeModules from './store';
+export Mixins from './mixins';
